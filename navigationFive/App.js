07a27ea461cbfa80421371d2b1d5e094149c127f
batuhan <batuhanonur29@gmail.com>
2020-05-12 03:57:14 +0300

    stack & tab navigator

--- navigationFive/App.js
+++ navigationFive/App.js
@@ -3,7 +3,10 @@ import { View, Text, StyleSheet, Image, TouchableOpacity, StatusBar } from 'reac
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 
 function HomeScreen({ navigation }) {
@@ -18,6 +21,8 @@ function HomeScreen({ navigation }) {
 
       <View style={{ flex: 2, alignItems: 'center' }}>
         <Text style={{ fontWeight: 'normal', fontSize: 32 }}>Home Screen</Text>
+
+        {/* diğer ekrana parametre geçirme */}
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
@@ -29,6 +34,17 @@ function HomeScreen({ navigation }) {
         >
           <Text style={{ color: '#FFF' }}>Go to the Music Screen</Text>
         </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.button,
+          { borderColor: "#23A6D9", borderWidth: 1, marginTop: 12, backgroundColor: "#FFF" },
+          ]}
+          onPress={() => {
+            navigation.navigate("Settings");
+          }}
+        >
+          <Text style={{ color: "#23A6D9" }}>Go to Settings</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -37,10 +53,13 @@ function HomeScreen({ navigation }) {
 
 function MusicScreen({ route, navigation }) {
 
+  {/* önceki ekrandan gelen parametreler */ }
   let { userName, action } = route.params;
 
+  {/* like butonu için state tanımı */ }
   const [liked, setLiked] = React.useState(false);
 
+  {/* header için layout özellikleri (sağ tarafa kalp butonu) */ }
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -66,10 +85,13 @@ function MusicScreen({ route, navigation }) {
 
       <View style={{ flex: 2, alignItems: 'center' }}>
         <Text style={{ fontWeight: 'normal', fontSize: 32 }}>Music Screen</Text>
+
+        {/* Önceki sayfadan gelen parametreler */}
         <Text style={{ fontWeight: "600", marginVertical: 32 }}>
           {userName} says to {action}
         </Text>
 
+        {/* Home sayfasına gitme */}
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
@@ -80,6 +102,7 @@ function MusicScreen({ route, navigation }) {
           <Text style={{ color: '#FFF' }}>Go to the Home Screen</Text>
         </TouchableOpacity>
 
+        {/* goBack butonu ile bir önceki sayfaya dönme*/}
         <TouchableOpacity
           style={[
             styles.button, {
@@ -102,21 +125,86 @@ function MusicScreen({ route, navigation }) {
   )
 }
 
-
+{/* Custom header için fonksiyon */ }
 function CustomHeader() {
   return <Icon name="music" size={24} color="#FFF" />
 }
 
 
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={{ width: "100%", flex: 1, marginTop: 64 }}>
+        <Image source={require("./assets/settings.png")}
+          style={{ width: undefined, height: undefined, flex: 1 }}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={{ flex: 2, alignItems: 'center' }}>
+        <Text style={{ fontWeight: "100", fontSize: 32 }}>Settings Screen</Text>
+      </View>
+    </View>
+  )
+}
+
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={{ width: "100%", flex: 1, marginTop: 64 }}>
+        <Image source={require("./assets/details.png")}
+          style={{ width: undefined, height: undefined, flex: 1 }}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={{ flex: 2, alignItems: 'center' }}>
+        <Text style={{ fontWeight: "100", fontSize: 32 }}>Details Screen</Text>
+      </View>
+    </View>
+  )
+}
+
+/* create navigator */
 const Main = createStackNavigator();
+const Settings = createBottomTabNavigator();
+
+
+function SettingsTab() {
+  return (
+
+
+    <Settings.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        if (route.name === 'SettingsScreen') {
+          return <Icon name="cog" color={color} size={size} />
+        }
+
+        return <Icon name="paperclip" size={size} color={color} />
+      }
+    })}
+      tabBarOptions={{
+        activeTintColor: "#23A6D9",
+        inactiveTintColor: "gray"
+      }}
+
+    >
+      <Settings.Screen name="SettingsScreen" component={SettingsScreen} />
+      <Settings.Screen name="DetailsScreen" component={DetailsScreen} />
+    </Settings.Navigator>
+  )
+}
 
 
 
 const App = () => {
   return (
 
+
     <NavigationContainer >
+      {/* Status Bar özellikleri */}
       <StatusBar barStyle="light-content" backgroundColor="#23A6D9" />
+      {/* header özellikleri */}
       <Main.Navigator screenOptions={{
         headerStyle: {
           backgroundColor: "#23A6D9",
@@ -131,6 +219,8 @@ const App = () => {
         <Main.Screen name="Music" component={MusicScreen} options={{
           headerTitle: props => <CustomHeader {...props} />
         }} />
+
+        <Main.Screen name="Settings" component={SettingsTab} />
       </Main.Navigator>
     </NavigationContainer>
 
